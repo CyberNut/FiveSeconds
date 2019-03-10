a7e52b2c624ba8739d7d0854345f54e30f076aac
CyberNut <cy6ernut@gmail.com>
2019-03-10 11:00:55 +0300

    new try

--- app/src/main/java/ru/cybernut/fiveseconds/view/GameViewModel.java
+++ app/src/main/java/ru/cybernut/fiveseconds/view/GameViewModel.java
@@ -1,19 +1,4 @@
 package ru.cybernut.fiveseconds.view;
 
-import android.app.Application;
-import android.arch.lifecycle.AndroidViewModel;
-import android.arch.lifecycle.ViewModel;
-import android.support.annotation.NonNull;
-
-import ru.cybernut.fiveseconds.model.Game;
-
-public class GameViewModel extends AndroidViewModel {
-
-    private Game game;
-
-    public GameViewModel(@NonNull Application application, int numberOfQuestions) {
-        super(application);
-        game = new Game(application, Game.GameType.AUTO_PLAY_SOUND, numberOfQuestions);
-    }
-
+public class GameViewModel {
 }
