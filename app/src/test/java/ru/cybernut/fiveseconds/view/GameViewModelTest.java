b0af9769fcc9b1b17ba024e6b220e29f1ec3ddae
Andrey <cy6ernut@gmail.com>
2019-07-12 08:28:06 +0300

    Пробный юнит-тест

---
+++ app/src/test/java/ru/cybernut/fiveseconds/view/GameViewModelTest.java
@@ -0,0 +1,12 @@
+package ru.cybernut.fiveseconds.view;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GameViewModelTest {
+
+    @Test
+    public void initialize() {
+    }
+}
